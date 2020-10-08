8d36c2201b752110429ca6e5d7aed955b097a92b
Julio <julio@tripperbs.com>
2020-10-08 09:20:36 -0600

    se agregan routes y views

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: true
+  }));
+
+// middlewares
+app.use(morgan('dev'));
+
+// routes
+app.use(require('./routes'));
+
+// static files
+
+// listening the Server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+const fetch = require("node-fetch");
+
+const API_KEY = "ikwmC7O4fi1lVvGBE3mwcJ3jSSbGfZWDf9eHVnXD";
+const url = "https://api.nasa.gov/planetary/earth/assets?";
+const fecha = "2020-10-9";
+const dim = "0.055";
+
+const options = {
+  "method": "GET",
+};
+
+router.get('/', (req, res) => {
+  res.render('index.html', { title: 'RSM - Busqueda de fotografias aereas' });
+});
+
+router.post('/tiles', (req, res) => {
+  const coordenadas = req.body.coordenadas.split(',');
+
+  var lat = coordenadas[0]; 
+  var lon= coordenadas[1];
+  
+  if(process.argv.length==4) {
+    var parts = process.argv.slice(2);
+    lat = parts[0].replace(',','').trim();
+    lon = parts[1].replace(',','').trim();
+  } 
+//lon=-95.33&lat=29.78&date=2018-01-01&&dim=0.10&api_key="+API_KEY;
+  fetch(url+'lon='+lon+'&lat='+lat+'&date='+fecha+'&dim='+dim+'&api_key='+API_KEY, options)
+    .then(res => res.json()) // expecting a json response
+    .then((data) => {
+      const imagen = data.url;
+      res.render('tiles.html', { title: 'Coordenadas', img: imagen });
+    });
+
+});
+
+
+module.exports = router;
